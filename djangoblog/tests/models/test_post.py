e36de76004feb739eb9ceafc4546fe1d68b68da3
igorgbr <igorgphotoarte@gmail.com>
2021-10-16 20:36:38 -0300

    exercise

--- djangoblog/tests/models/test_post.py
+++ djangoblog/tests/models/test_post.py
@@ -5,9 +5,16 @@ from blog.factories import PostFactory
 
 @pytest.fixture
 def post_published():
-    return PostFactory(title="pytest with factory")
+    return PostFactory(
+        title="pytest with factory", status=1, content="that's MY test"
+    )
 
 
 @pytest.mark.django_db
 def test_create_published_post(post_published):
     assert post_published.title == "pytest with factory"
+    assert post_published.status == 1
+    assert post_published.author.username is not None
+    assert post_published.author.email is not None
+    assert post_published.content == "that's MY test"
+
